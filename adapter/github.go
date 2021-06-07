d63c7ef5a1a018bee0dc2347bac43fe793e8d4fb
ZrH-Source <71234118+ZrH-Source@users.noreply.github.com>
2021-06-07 21:02:08 +0200

    Adapter/GitHub (#5)
    * Creation du Serveur Web
    
    * github.go
    
    * Added secret to log the push event
    
    * Analyse of the webhook
    
    Co-authored-by: Zarioch <zariocgta@gmail.com>

--- adapter/github.go
+++ adapter/github.go
@@ -3,15 +3,21 @@ package adapter
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/go-playground/webhooks/v6/github"
 )
 
+type gitInfo struct {
+	author     string
+	repository string
+	email      string
+	hash       string
+}
+
 func GitHandler(w http.ResponseWriter, r *http.Request) {
 	hook, _ := github.New(github.Options.Secret("Whatever"))
 	payload, err := hook.Parse(r, github.PushEvent)
-
-	fmt.Println(payload)
 
 	if err != nil {
 		if err == github.ErrEventNotFound {
@@ -20,6 +26,11 @@ func GitHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	if push, v := payload.(github.PushPayload); v {
-		fmt.Printf("%+v", push)
+		str := push.Repository.FullName
+		repository := strings.Split(str, "/")[1]
+
+		infos := gitInfo{author: push.Pusher.Name, repository: repository, email: push.Pusher.Email, hash: push.Commits[0].ID}
+
+		fmt.Println(infos)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/zrh-source/GitHub-Notifier
 
 go 1.16
 
-require github.com/go-playground/webhooks/v6 v6.0.0-beta.3 // indirect
+require (
+	github.com/go-playground/webhooks/v6 v6.0.0-beta.3 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	gopkg.in/go-playground/webhooks.v5 v5.17.0 // indirect
+)
