b056594ba559d505f7d8147dec858f9a0855a627
Zarioch <zariocgta@gmail.com>
2021-06-23 16:41:13 +0200

    Added gitclone and gitpull in common.go

--- adapter/github.go
+++ adapter/github.go
@@ -8,13 +8,6 @@ import (
 	"github.com/go-playground/webhooks/v6/github"
 )
 
-type GitInfo struct {
-	author     string
-	repository string
-	email      string
-	hash       string
-}
-
 func GitHandler(w http.ResponseWriter, r *http.Request) {
 	hook, _ := github.New(github.Options.Secret("Whatever"))
 	payload, err := hook.Parse(r, github.PushEvent)
@@ -30,7 +23,7 @@ func GitHandler(w http.ResponseWriter, r *http.Request) {
 		repository := strings.Split(repoName, "/")[1]
 		infos := GitInfo{author: push.Pusher.Name, repository: repository, email: push.Pusher.Email, hash: push.Commits[0].ID}
 		fmt.Println(infos)
-		PullPayload()
+		infos.Update()
 	}
 
 }
